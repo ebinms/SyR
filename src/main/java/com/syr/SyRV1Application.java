1165cf8ed97f5d811c337c8bd304b008355add5f
ebinms <ebinms@gmail.com>
2018-12-29 16:29:13 +0530

    Character encoding changes

--- src/main/java/com/syr/SyRV1Application.java
+++ src/main/java/com/syr/SyRV1Application.java
@@ -2,8 +2,16 @@ package com.syr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
 
+@Configuration
+@RestController
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class SyRV1Application {
 
 	public static void main(String[] args) {
